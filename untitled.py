6b8e2761e613b48cb51477486b99b9ae3c376bf8
S-tuberosum <okhaledn@gmail.com>
2021-03-23 07:35:27 -0500

    improved animation

--- untitled.py
+++ untitled.py
@@ -2,26 +2,26 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.integrate as sp
+import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 #plt.rcParams['axes.facecolor'] = 'black'
 
 steps = 10000  # time steps
-dt = 1e-2  # dt value for Euler's method
+dt = 0.05  # dt value for Euler's method
 
 ## Constants
 G = 1  # gravitational constant
 
 ## Sphere generation
 n_0 = 1  # number of black holes
-black = np.ones((n_0, 7)) * [300000, 0, 0, 0, 1, 1, 1]  # black hole array
+black = np.ones((n_0, 7)) * [300000, 0, 0, 0, 0, 0, 0]  # black hole array
 
-n = 100  # number of spheres
+n = 2000  # number of spheres
 sphere = np.ones((n, 7))  # array to store sphere objects with a mass and position attribute respectively
-sphere[:, 1:4] = sphere[:, 1:4] * np.random.randint(-100, 100,
-                                                    size=(n, 3))  # giving each sphere a random momentum from -100, 100
-sphere[:, 4:] = sphere[:, 4:] * np.random.randint(-50, 50,
-                                                  size=(n, 3))  # giving each sphere random positions from -50, 50
+sphere[:, 4:] = sphere[:, 4:] * np.random.randint(30, 60, size=(n, 3))
+sphere[:, 1:4] = sphere[:, 1:4] * np.sqrt(G * black[0, 0]/(np.linalg.norm(sphere[:, 4:], axis=1)))[:, None]/2
+sphere[:, 2:3] = sphere[:, 2:3] * -1
+sphere[:, 3:4] = 0
 
 
 def force():
@@ -39,25 +39,22 @@ def force():
 
 fig = plt.figure(figsize=(7, 7))
 ax = fig.add_subplot(1, 1, 1, projection='3d', xlim=(-100, 100), ylim=(-100, 100), zlim=(-100, 100))
-# ax.set_xticks([])
-# ax.set_yticks([])
-# ax.set_zticks([])
+ax.set_xticks([])
+ax.set_yticks([])
+ax.set_zticks([])
 # ax.grid(False)
 # ax.axis('off')
 
-for i in range(steps):
-    # ax.set_xlim3d(black[:, 4] - 100, black[:, 4] + 100)
-    # ax.set_ylim3d(black[:, 5] - 100, black[:, 6] + 100)
-    # ax.set_zlim3d(black[:, 5] - 100, black[:, 6] + 100)
+ax.scatter3D(black[:, 4], black[:, 5], black[:, 6], color='black')
+graph = ax.scatter3D(sphere[:, 4], sphere[:, 5], sphere[:, 6], color="b", alpha=0.5, s=1)
+
+
+def update(i):
     sphere[:, 1:4] = sphere[:, 1:4] + dt * force()
     sphere[:, 4:] = sphere[:, 4:] + dt * sphere[:, 1:4] / (sphere[:, 0][:, None])
-    #black[:, 4:] = black[:, 4:] + dt * black[:, 1:4] / (black[:, 0][:, None])
-    ax.scatter3D(black[:, 4], black[:, 5], black[:, 6], color='black')
-    ax.scatter3D(sphere[:, 4], sphere[:, 5], sphere[:, 6], color="b", alpha=0.5, s=5)
-    plt.show(block=False)
-    plt.pause(dt)
-    #pts.remove()
-    del ax.collections[:]
-    # plt.clf()
+    graph._offsets3d = (sphere[:, 4], sphere[:, 5], sphere[:, 6])
 
-# Do correlation
+
+ani = animation.FuncAnimation(fig, update, steps, interval=1)
+plt.show()
+
